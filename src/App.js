dbbc2ea0fb370a1bf112f46211707ce54dcba835
Nikolai Nanev <niki4etooo@gmail.com>
2023-09-12 21:13:42 +0300

    todo: added the four columns for each phase

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Simple Task Board
-      </header>
+
+      <div className="container">
+        <div className="column" id="column-story"></div>
+        <div className="column" id="column-progress"></div>
+        <div className="column" id="column-test"></div>
+        <div className="column" id="column-done"></div>
+      </div>
     </div>
   );
 }
